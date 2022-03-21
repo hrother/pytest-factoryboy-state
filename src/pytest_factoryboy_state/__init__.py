cb741388ab8714aa6612f7c44fa7ebff6920bfae
Holger Rother <hrother@hrother.org>
2022-03-21 01:43:31 +0100

    Add pyupgrade to pre-commit hooks

--- src/pytest_factoryboy_state/__init__.py
+++ src/pytest_factoryboy_state/__init__.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from pytest_factoryboy_state.plugin import pytest_addoption
 from pytest_factoryboy_state.plugin import pytest_sessionstart
 from pytest_factoryboy_state.plugin import pytest_terminal_summary
